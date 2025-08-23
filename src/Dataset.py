16b967e3b5d7d8e376794e18857a5a268b58f56e
Piotr Świebocki <swpiotr5@interia.pl>
2025-08-23 16:11:22 +0200

    Dataset class update - load prepared images and masks

--- src/Dataset.py
+++ src/Dataset.py
@@ -3,35 +3,50 @@ import glob
 import numpy as np
 
 class Dataset():
-    def __init__(self, person_dir_path):
-        self.data_path = "data/prepared"
-        self.person_dir = os.path.join(self.data_path, person_dir_path)
-        self.images = glob.glob(f"{self.person_dir}\\ct\\slice_*.npy",recursive=True)
-        self.masks = glob.glob(f"{self.person_dir}\\mask\\slice_*.npy", recursive=True)
-        self.images.sort()
-        self.masks.sort()
+    def __init__(self, base_dir="../data/prepared", transformation=None, data_type = ['liver', 'liver_tumor', 'tumor']):
+        self.images = []
+        self.masks = []
+        self.transformation = transformation
+        self.data_type = data_type
+
+        for person_dir in sorted(os.listdir(base_dir)):
+            if "person" in person_dir:
+                for data_type in self.data_type:
+                    data_type_dir = os.path.join(base_dir, person_dir, data_type)
+                    is_ct_and_mask = os.listdir(data_type_dir)
+                    if "ct" in is_ct_and_mask:
+                        ct_dir = os.path.join(data_type_dir, "ct")
+                        for ct_scan in sorted(os.listdir(ct_dir)):
+                            ct_path = os.path.join(ct_dir, ct_scan)
+                            # check if file is valid npy
+                            try:
+                                arr = np.load(ct_path)
+                                self.images.append(ct_path)
+                            except Exception as e:
+                                print(f"Pominięto uszkodzony plik CT: {ct_path}, błąd: {e}")
+                    if "mask" in is_ct_and_mask:
+                        mask_dir = os.path.join(data_type_dir, "mask")
+                        for mask in sorted(os.listdir(mask_dir)):
+                            mask_path = os.path.join(mask_dir, mask)
+                            try:
+                                arr = np.load(mask_path)
+                                self.masks.append(mask_path)
+                            except Exception as e:
+                                print(f"Pominięto uszkodzony plik maski: {mask_path}, błąd: {e}")
 
     def __getitem__(self, idx):
-        image_path = self.images[idx]
-        mask_path = self.masks[idx]
+        image = np.load(self.images[idx])
+        mask  = np.load(self.masks[idx])
 
-        image = np.load(image_path)        
-        mask = np.load(mask_path)
+        if image.ndim == 2:
+            image = np.expand_dims(image, axis=-1)
+
+        if self.transformation:
+            augmented = self.transformation(image=image, mask=mask)
+            image, mask = augmented["image"], augmented["mask"]
 
         return image, mask
 
-class LiverDataset(Dataset):
-    def __init__(self, person_id):
-        super().__init__(f"person_{person_id}\\liver")
-   
-class TumorDataset(Dataset):
-    def __init__(self, person_id):
-        super().__init__(f"person_{person_id}\\tumor")
+    def __len__(self):
+        return len(self.images)
 
-class LiverTumorDataset(Dataset):
-    def __init__(self, person_id):
-        super().__init__(f"person_{person_id}\\liver_tumor")
-
-class BgOnlyDataset(Dataset):
-    def __init__(self, person_id):
-        super().__init__(f"person_{person_id}\\background_only")
